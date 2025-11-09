686d7192ca6956636e20252d9a8aa011e34e19b0
gustimaulanid <gmaulan47@gmail.com>
2025-11-09 23:55:44 +0700

    fix: url proxy

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8787',
+        target: 'http://localhost:8787' || import.meta.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
